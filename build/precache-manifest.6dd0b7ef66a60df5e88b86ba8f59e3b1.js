b278946a8583fbe3356eea013aa93438f0960090
EC2 Default User <ec2-user@ip-172-31-80-84.ec2.internal>
2019-12-30 20:16:25 +0000

    prod

---
+++ build/precache-manifest.6dd0b7ef66a60df5e88b86ba8f59e3b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a967289332b6584ad8063defc0b1af2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0ff08ce3fc295c8287d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b41d7f39f4bcc82f08d1",
+    "url": "/static/js/2.38e36d6f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.38e36d6f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0ff08ce3fc295c8287d",
+    "url": "/static/js/main.ff4564ba.chunk.js"
+  },
+  {
+    "revision": "77a84e0f07871fe94f1f",
+    "url": "/static/js/runtime-main.a8258d1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
